52970a82ff7ccee6e8eff3600b3b33f2a563fb20
grobastic <matkov.sergey@gmail.com>
2014-12-08 23:03:20 +0800

    Добавлено создание некоторых таблиц
    userpermits, points, articles, feedback

--- start.php
+++ start.php
@@ -2,17 +2,17 @@
 <?php
 require_once 'lib/sqlvariable.php';
 require_once 'lib/sql.php';
-function new_table_users () {
+function mysql_users () {
     // автоматическое создание таблицы пользователей в базе
  if (mysql_query("SELECT * FROM users LIMIT 1")) { echo 'Таблица <b>users</b> уже есть в базе!<br>'; }
  else {
-    mysql_query('CREATE TABLE IF NOT EXISTS users (userid INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, user_login VARCHAR(30) NOT NULL, user_password TINYTEXT NOT NULL, user_ip INT(10), user_hash VARCHAR(32), user_email TINYTEXT, user_cat TINYTEXT, user_phone1 TINYTEXT, user_address TEXT, user_inn TINYINT, user_ogrn TINYINT, datereg TIMESTAMP, datechange TIMESTAMP, reserv1 TEXT, reserv2 TEXT, reserv3 INT, reserv4 INT)')
+    mysql_query('CREATE TABLE IF NOT EXISTS users (userid INT(11) AUTO_INCREMENT PRIMARY KEY NOT NULL, user_login VARCHAR(30) NOT NULL, user_password TINYTEXT NOT NULL, user_ip TEXT(12), user_hash VARCHAR(32), user_email TINYTEXT, user_cat TINYTEXT, user_phone1 TINYTEXT, user_address TEXT, user_inn TINYINT, user_ogrn TINYINT, datereg TIMESTAMP, datechange TIMESTAMP, reserv1 TEXT, reserv2 TEXT, reserv3 INT, reserv4 INT)')
     or die ("SQL Error: ".  mysql_error()); 
     echo 'Таблица <b>users</b> успешно создана!<br>';
  }
 }
 
-function new_table_trackers () {
+function mysql_trackers () {
     // автоматическое создание таблицы трекеров в базе
  if (mysql_query("SELECT * FROM trackers LIMIT 1")) { echo 'Таблица <b>trackers</b> уже есть в базе!<br>'; }
 else {
@@ -22,7 +22,7 @@ else {
     }
 }
 
-function userpermits () {
+function mysql_userpermits () {
     // автоматическое создание таблицы прав пользователей в базе
  if (mysql_query("SELECT * FROM userpermits LIMIT 1")) { echo 'Таблица <b>userpermits</b> уже есть в базе!<br>'; }
 else {
@@ -41,7 +41,7 @@ else {
     }
 }
 
-function articles () {
+function mysql_articles () {
     // автоматическое создание таблицы статей в базе
 if (mysql_query("SELECT * FROM articles LIMIT 1")) { echo 'Таблица <b>articles</b> уже есть в базе!<br>'; }
 else {
@@ -51,10 +51,34 @@ else {
     }
 }
 
-new_table_users ();
-userpermits ();
-new_table_trackers ();
-articles ();
+function mysql_feedback (){
+    // автоматическое создание таблицы отзывов в базе
+    if (mysql_query("SELECT * FROM feedback LIMIT 1")) { echo 'Таблица <b>feedback</b> уже есть в базе!<br>'; }
+    else {
+        mysql_query('CREATE TABLE IF NOT EXISTS feedback (feedbackid INT AUTO_INCREMENT PRIMARY KEY NOT NULL, fid_userid INT, fid_fio TEXT(50), fid_phone TEXT (15), fid_email TINYTEXT, fid_text_message TEXT, fid_browser TEXT, fid_user_ip TEXT(12), fid_date TIMESTAMP NOT NULL, fid_info TEXT)')or die ("SQL Error: ".  mysql_error());
+    echo 'Таблица <b>feedback</b> успешно создана!<br>';
+    }
+    
+}
+
+function mysql_points () {
+    // автоматическое создание таблицы трекеров в базе
+ if (mysql_query("SELECT * FROM points LIMIT 1")) { echo 'Таблица <b>points</b> уже есть в базе!<br>'; }
+else {
+    mysql_query('CREATE TABLE IF NOT EXISTS points (ptid INT AUTO_INCREMENT PRIMARY KEY NOT NULL, imei INT(20) NOT NULL, device_id INT(20) NOT NULL, timestamp_utc TIMESTAMP NOT NULL, '
+            . 'latitude DECIMAL(15,13) NOT NULL, longitude DECIMAL(15,13) NOT NULL, satellites INT(2), speed INT (3), angle INT(3), gps_state TEXT(5), mcc TEXT(4), mnc TEXT(4), '
+            . 'lac TEXT(5), cellid TEXT(8), state_acc TEXT(11), state_power TEXT(11), state_gps TEXT(11), state_oil TEXT(11), vin INT(5), gsm INT(3))')
+    or die ("SQL Error: ".  mysql_error()); 
+    echo 'Таблица <b>points</b> успешно создана!<br>';
+    }
+}
+
+mysql_users ();
+mysql_userpermits ();
+mysql_trackers ();
+mysql_articles ();
+mysql_feedback ();
+mysql_points ();
 
 ?>
 <a href="index.php">Перейти на главную</a>
